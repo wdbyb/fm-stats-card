827207b752cf8bb6ce5fb76eeea5bd2724de47ea
Вини <yvinivitin@gmail.com>
2022-12-12 17:02:39 +0200

    card finished

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./index.html', './*.js'],
+  theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      veryDark: 'hsl(233, 47%, 7%)',
+      darkBlue: 'hsl(244, 38%, 16%)',
+      softViolet: 'hsl(277, 64%, 61%)',
+    },
+    fontFamily: {
+      sansHeading: ['Inter', 'sans-serif'],
+      sansText: ['Lexend Deca', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
